a18bee35b7cba73203092f10e18384ddd5d93be1
Mark Bellott <mdbellott@gmail.com>
2019-10-19 13:58:47 -0400

    xxv

---
+++ 2019.10/15/xxv.js
@@ -0,0 +1,32 @@
+// xxv
+// Mark Bellott
+//
+// p5.js
+// Rendered on openprocessing.org
+
+let size = 10
+let dist = 35
+
+let theta = 0.0
+
+function setup() {
+	createCanvas(700, 700)
+	background(0)
+	stroke(255)	
+	noFill()
+}
+
+function draw() {
+	background(0)
+
+	// Animate
+	for(let i=-10; i<30; i++) {		
+		for(let j=-10; j<30; j++) {
+			let x = 1.5*dist + dist*i + dist*sin(theta + i)/2
+			let y = 1.5*dist + dist*j + dist*cos(theta + j)/2
+			ellipse(x, y, size, size)
+		}
+	}	
+	theta += 0.03
+}
+
