4aab29cae127011ea076307069237e682c0b69e5
Mark Bellott <mdbellott@gmail.com>
2020-04-09 16:30:34 -0400

    4 / 7-9 / 20

---
+++ 2020.04/09/Sand.js
@@ -0,0 +1,63 @@
+// Sand
+// Mark Bellott
+
+// Inspiration from:
+// https://www.openprocessing.org/sketch/494102
+
+var particles = []
+
+let n = 200
+let noiseScale = -3.125
+
+let speed = 7
+let radius = 1
+
+let W = 700
+let H = 700
+
+function setup() {
+  createCanvas(W, H)
+  background(0)
+  
+  for(var i=0; i<n; i++) {
+    particles[i] = new Particle(random(0, W),random(0,H))
+  }
+}
+
+function draw() {
+  noStroke()
+  smooth()
+  for(var i=0; i<n; i++) {
+    fill(255,255,255,255)
+    particles[i].move()
+    particles[i].display(radius)
+    particles[i].checkEdge()
+  }
+}
+
+function Particle(x,y) {
+  this.dir = createVector(0,0)
+  this.vel = createVector(0,0)
+  this.pos = createVector(0,0)
+  this.speed = speed
+  
+  this.move = function() {
+    var angle = noise(this.pos.x/noiseScale, this.pos.y/noiseScale)*PI*noiseScale
+    this.dir.x = cos(angle)
+    this.dir.y = sin(angle)
+    this.vel = this.dir.copy()
+    this.vel.mult(this.speed)
+    this.pos.add(this.vel)
+  }
+  
+  this.checkEdge = function(){
+    if(this.pos.x > width || this.pos.x < 0 || this.pos.y > height || this.pos.y < -50){
+      this.pos.x = random(-500, width)
+      this.pos.y = random(-500, height)
+    }
+  }
+  
+  this.display = function(r){
+    ellipse(this.pos.x, this.pos.y, r, r)
+  }
+}
